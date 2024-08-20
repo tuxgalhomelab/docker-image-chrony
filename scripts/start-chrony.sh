46ee041a5fda09611da64700095396ab30052e2a
G3 <tuxgal@Baymax.area51>
2024-08-20 10:06:19 -0700

    feat: Remove the chrony pid file if it exists.
    This can happen when we have an abrupt shutdown and docker
    daemon restarts all the containers using the previous images.

--- scripts/start-chrony.sh
+++ scripts/start-chrony.sh
@@ -2,6 +2,7 @@
 set -E -e -o pipefail
 
 chrony_config="/data/chrony/chrony.conf"
+chrony_pid_file="/var/run/chrony/chronyd.pid"
 
 set_umask() {
     # Configure umask to allow write permissions for the group by default
@@ -13,6 +14,7 @@ start_chrony() {
     echo "Starting Chrony ..."
     echo
 
+    rm -f ${chrony_pid_file:?}
     exec /usr/sbin/chronyd \
         -d \
         -f /data/chrony/chrony.conf \
